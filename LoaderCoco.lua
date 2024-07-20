f243557b005e96662c9a915f0dda275b883a04e8
OneCreatorX-New <cuentapanama19@gmail.com>
2024-07-20 18:46:38 -0300

    Create LoaderCoco.lua

---
+++ LoaderCoco.lua
@@ -0,0 +1,34 @@
+return function(id)
+    local function notify(message)
+        pcall(function()
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Notify",
+                Text = message,
+                Duration = 5
+            })
+        end)
+    end
+
+    local function loadScript()
+        local success, result = pcall(function()
+            return game:HttpGet("https://raw.githubusercontent.com/CocoJale/Scripts/main/" .. id .. ".lua")
+        end)
+        
+        if success then
+            local func, loadErr = loadstring(result)
+            if func then
+                pcall(func)
+            else
+                notify("Error compiling Script: " .. tostring(loadErr))
+            end
+        else
+            notify("Error loading Script: " .. tostring(result))
+        end
+    end
+
+    if tonumber(id) and tonumber(id) ~= game.PlaceId then
+        notify("Possible script for another game")
+    else
+        loadScript()
+    end
+end
